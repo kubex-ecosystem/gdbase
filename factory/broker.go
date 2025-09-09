0246fde3a1b157a976856bcbd35b9216804109eb
Rafael Mori <faelmori@gmail.com>
2025-09-09 16:47:09 -0300

    feat: Add media optimization scripts and enhance platform detection
    - Introduced `optmize_media.sh` for optimizing media files in the build process.
    - Enhanced `platform.sh` to improve platform and architecture detection, including better handling of various architecture names.
    - Added `0_post_build.sh.txt` and `0_pre_build.sh.txt` scripts for pre and post build processes with error handling.
    - Updated `utils.sh` for improved logging and error handling, including better management of temporary directories.
    - Refactored `validate.sh` to modularize Go version checks and improve dependency validation from manifest files.
    
    Signed-off-by: Rafael Mori <faelmori@gmail.com>

--- factory/broker.go
+++ factory/broker.go
@@ -4,83 +4,75 @@ package factory
 ////go:build !windows
 //// +build !windows
 
-import (
-	"log"
-	"runtime"
+// type Broker = services.BrokerImpl
+// type BrokerInfo = services.BrokerInfoLock
+// type BrokerManager = services.BrokerManager
 
-	"github.com/kubex-ecosystem/gdbase/internal/services"
-	"github.com/streadway/amqp"
-)
+// func NewBrokerService(verbose bool, port string) (*Broker, error) {
+// 	if runtime.GOOS == "windows" {
+// 		log.Println("Broker service is not supported on Windows.")
+// 		return nil, nil
+// 	}
+// 	return services.NewBroker(verbose)
+// }
+// func NewBrokerManager() *BrokerManager {
+// 	if runtime.GOOS == "windows" {
+// 		log.Println("Broker service is not supported on Windows.")
+// 		return nil
+// 	}
+// 	return services.NewBrokerManager()
+// }
+// func NewBrokerInfo(port string) *BrokerInfo {
+// 	if runtime.GOOS == "windows" {
+// 		log.Println("Broker service is not supported on Windows.")
+// 		return nil
+// 	}
+// 	return services.NewBrokerInfo("", port)
+// }
 
-type Broker = services.BrokerImpl
-type BrokerInfo = services.BrokerInfoLock
-type BrokerManager = services.BrokerManager
+// func PublishMessage(queueName string, message string) error {
+// 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+// 	if err != nil {
+// 		log.Printf("Erro ao conectar ao RabbitMQ: %s", err)
+// 		return err
+// 	}
+// 	defer conn.Close()
 
-func NewBrokerService(verbose bool, port string) (*Broker, error) {
-	if runtime.GOOS == "windows" {
-		log.Println("Broker service is not supported on Windows.")
-		return nil, nil
-	}
-	return services.NewBroker(verbose)
-}
-func NewBrokerManager() *BrokerManager {
-	if runtime.GOOS == "windows" {
-		log.Println("Broker service is not supported on Windows.")
-		return nil
-	}
-	return services.NewBrokerManager()
-}
-func NewBrokerInfo(port string) *BrokerInfo {
-	if runtime.GOOS == "windows" {
-		log.Println("Broker service is not supported on Windows.")
-		return nil
-	}
-	return services.NewBrokerInfo("", port)
-}
+// 	ch, err := conn.Channel()
+// 	if err != nil {
+// 		log.Printf("Erro ao abrir um canal: %s", err)
+// 		return err
+// 	}
+// 	defer ch.Close()
 
-func PublishMessage(queueName string, message string) error {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	if err != nil {
-		log.Printf("Erro ao conectar ao RabbitMQ: %s", err)
-		return err
-	}
-	defer conn.Close()
+// 	q, err := ch.QueueDeclare(
+// 		queueName,
+// 		false,
+// 		false,
+// 		false,
+// 		false,
+// 		nil,
+// 	)
+// 	if err != nil {
+// 		log.Printf("Erro ao declarar a fila: %s", err)
+// 		return err
+// 	}
 
-	ch, err := conn.Channel()
-	if err != nil {
-		log.Printf("Erro ao abrir um canal: %s", err)
-		return err
-	}
-	defer ch.Close()
+// 	err = ch.Publish(
+// 		"",
+// 		q.Name,
+// 		false,
+// 		false,
+// 		amqp.Publishing{
+// 			ContentType: "text/plain",
+// 			Body:        []byte(message),
+// 		},
+// 	)
+// 	if err != nil {
+// 		log.Printf("Erro ao publicar a mensagem: %s", err)
+// 		return err
+// 	}
 
-	q, err := ch.QueueDeclare(
-		queueName,
-		false,
-		false,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		log.Printf("Erro ao declarar a fila: %s", err)
-		return err
-	}
-
-	err = ch.Publish(
-		"",
-		q.Name,
-		false,
-		false,
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(message),
-		},
-	)
-	if err != nil {
-		log.Printf("Erro ao publicar a mensagem: %s", err)
-		return err
-	}
-
-	log.Printf("Mensagem publicada na fila %s: %s", queueName, message)
-	return nil
-}
+// 	log.Printf("Mensagem publicada na fila %s: %s", queueName, message)
+// 	return nil
+// }
--- go.mod
+++ go.mod
@@ -8,16 +8,15 @@ require (
 	github.com/fatih/color v1.18.0
 	github.com/goccy/go-json v0.10.5
 	github.com/google/uuid v1.6.0
-	github.com/opencontainers/image-spec v1.1.1
-	github.com/pebbe/zmq4 v1.4.0
-	github.com/pelletier/go-toml/v2 v2.2.4
 	github.com/kubex-ecosystem/logz v1.5.0
+	github.com/opencontainers/image-spec v1.1.1
+	github.com/pelletier/go-toml/v2 v2.2.4
 	github.com/spf13/cobra v1.10.1
-	github.com/spf13/viper v1.20.1
+	github.com/spf13/viper v1.21.0
 	github.com/stretchr/testify v1.11.1
 	github.com/subosito/gotenv v1.6.0
 	github.com/zalando/go-keyring v0.2.6
-	golang.org/x/crypto v0.41.0
+	golang.org/x/crypto v0.42.0
 	gopkg.in/ini.v1 v1.67.0
 	gopkg.in/yaml.v3 v3.0.1
 	gorm.io/driver/mysql v1.6.0
@@ -27,7 +26,8 @@ require (
 )
 
 require (
-	github.com/bytedance/sonic v1.14.0 // indirect
+	github.com/bytedance/gopkg v0.1.3 // indirect
+	github.com/bytedance/sonic v1.14.1 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/containerd/errdefs v1.0.0 // indirect
@@ -45,8 +45,9 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
-	golang.org/x/arch v0.20.0 // indirect
-	golang.org/x/net v0.43.0 // indirect
+	go.yaml.in/yaml/v3 v3.0.4 // indirect
+	golang.org/x/arch v0.21.0 // indirect
+	golang.org/x/net v0.44.0 // indirect
 )
 
 require (
@@ -80,7 +81,8 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/kubex-ecosystem/xtui v1.3.2
+	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
@@ -93,13 +95,12 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/kubex-ecosystem/xtui v1.3.1
 	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/sagikazarmark/locafero v0.10.0 // indirect
+	github.com/sagikazarmark/locafero v0.11.0 // indirect
 	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
-	github.com/spf13/afero v1.14.0 // indirect
-	github.com/spf13/cast v1.9.2 // indirect
-	github.com/spf13/pflag v1.0.9 // indirect
+	github.com/spf13/afero v1.15.0 // indirect
+	github.com/spf13/cast v1.10.0 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/streadway/amqp v1.1.0
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
@@ -110,10 +111,10 @@ require (
 	go.opentelemetry.io/otel/trace v1.38.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.6.0 // indirect
 	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6 // indirect
-	golang.org/x/sync v0.16.0 // indirect
-	golang.org/x/sys v0.35.0 // indirect
-	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
 	golang.org/x/time v0.11.0 // indirect
-	google.golang.org/protobuf v1.36.8 // indirect
+	google.golang.org/protobuf v1.36.9 // indirect
 	gotest.tools/v3 v3.5.2 // indirect
 )
